d9f2017a0b440203b677ae3e889410a2b93b900a
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-10-22 17:32:35 +0600

    test

--- client/components/ui/input.tsx
+++ client/components/ui/input.tsx
@@ -2,7 +2,9 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+// eslint-disable-next-line
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
